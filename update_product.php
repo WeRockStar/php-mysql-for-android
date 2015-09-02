7b0a5f9220880bf45d0398f52b798b61c045d641
WeRockStar <vr_freestyle@outlook.com>
2015-09-02 20:47:18 +0700

    update data products

---
+++ update_product.php
@@ -0,0 +1,31 @@
+<?php
+  $response = array();
+
+  if(isset($_POST['pid']) && isset($_POST['name']) && isset($_POST['price']) && isset($_POST['description'])) {
+    $pid = $_POST['pid'];
+    $name = $_POST['name'];
+    $price = $_POST['price'];
+    $description = $_POST['description'];
+
+    require_once "db_connection.php";
+
+    $db = new DB_CONNECT();
+    $sql = "UPDATE products SET name = '$name', price = '$price', description = '$description' WHERE pid = $pid";
+    $result = mysqli_query($db , $sql);
+
+    //check valid
+    if($result) {
+      $response["success"] = 1;
+      $response["message"] = "Products successfully updated.";
+
+      //to json
+      echo json_encode($response);
+    }else { }
+  }else {
+    //data is missing
+    $response["success"] = 0;
+    $response["message"] = "Required field is missing";
+
+    echo json_encode($response);
+  }
+ ?>
